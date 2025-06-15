82537fdb9ab5543e2cbd6fc7a8766b997b224044
panelenelo <panelenelo@gmail.com>
2025-06-14 23:44:32 -0300

    study 2025/06/14

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const port = 3000,
     http = require("http"),
     httpStatus = require("http-status-codes"),
-    app = http.createServer((request, response) =>{
-        console.log("Received request");
-        response.writeHead(httpStatus.OK, {
-            "Content-Type": "text/html"
-        });
+    app = http.createServer();
 
-        let responseMessage = "<h1>Hello</h1>";
-        response.write(responseMessage);
-        response.end();
-        console.log(`Sent a response: ${responseMessage}`);
+app.on("request", (req, res) => {
+    res.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html"
     });
 
-    app.listen(port);
-    console.log(`Server listening on ${port}`);
+    let responseMessage = "<h1>This will show on the screen.</h1>";
+    res.end(responseMessage);
+});
+
+app.listen(port);
+console.log(`Server listening on ${port}`);
+
