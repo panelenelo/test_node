5361518320b9e528cc6b04ff94b833278f0524a9
panelenelo <panelenelo@gmail.com>
2025-07-08 21:23:58 -0300

    study 2025/07/08

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const port = 3000,
 app.get("/", (req, res) => {
     res.send("Hi");
 })
+
+app.get("/contact", (req, res) => {
+    res.send("Contact infromation submitted successfully.");
+})
+
 .listen(port, () => {
     console.log(`server listening on port ${port}`);
 });
