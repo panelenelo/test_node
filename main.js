32582024e57e588bbfeac7d6f24443c64b78bae6
panelenelo <panelenelo@gmail.com>
2025-07-10 19:53:41 -0300

    study 2025/07/10

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const port = 3000,
     express = require("express"),
     app = express();
 
+/*
 app.get("/", (req, res) => {
     res.send("Hi");
 })
@@ -10,7 +11,24 @@ app.get("/items/:vegetable", (req, res) => {
     let veg = req.params.vegetable;
     res.send(`Request for ${veg}\n`);
 })
+*/
 
-.listen(port, () => {
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    res.send("POST Successfull!\n");
+});
+
+app.listen(port, () => {
     console.log(`server listening on port ${port}`);
 });
+
+
