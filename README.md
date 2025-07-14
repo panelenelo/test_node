9cec3a4b317d8e79a3e7ce46bffddd41e589787f
panelenelo <panelenelo@gmail.com>
2025-07-14 17:24:33 -0300

    study 2025/07/14

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Test and learning node
 test and learning node
+
+Clone repo and install with "npm ci" to have the same version of the packages used in the repo.
--- main.js
+++ main.js
@@ -31,6 +31,26 @@ app.post("/", (req, res) => {
     res.send("POST Successfull!\n");
 });
 
+app.get('/user/:id', (req, res, next) => {
+  console.log('ID:', req.params.id)
+  next()
+}, (req, res, next) => {
+  res.send('User Info')
+})
+
+// handler for the /user/:id path, which prints the user ID
+app.get('/user/:id', (req, res, next) => {
+  res.send(req.params.id)
+})
+
+function fob(req, res) {
+    console.log(req.originalUrl);
+    res.send('Mango');
+}
+
+app.get('/base', fob);
+
+
 app.listen(port, () => {
     console.log(`server listening on port ${port}`);
 });
