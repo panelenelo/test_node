46708e8a925cc540270f9320fd09631422856858
panelenelo <panelenelo@gmail.com>
2025-07-17 20:44:29 -0300

    study 2025/07/17

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,29 @@
-
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
 
 exports.sendReqParam = (req, res) => {
     let veg = req.params.vegetable;
-    res.send(`This is the page for ${veg}`);
+    res.send(`This is the page for ${veg}\n`);
 }
 
 exports.sendHi = (req, res) => {
     console.log(req.url);
-    res.send(`Hi Mango!`);
+    res.send(`Hi Mango!\n`);
+}
+
+exports.userSignUpProcessor = (req, res) => {
+    console.log(req.url);
+    console.log(req.body);
+    res.send(`POST done\n`);
+}
+
+exports.checkItems = (req, res, next) => {
+    console.log("visited items.\n");
+    next();
+}
+
+exports.respondWithName = (req, res) => {
+    res.render("index");
 }
--- main.js
+++ main.js
@@ -2,17 +2,7 @@ const port = 3000,
     express = require("express"),
     app = express();
     homeController = require("./controllers/homeController");
-
-/*
-
-app.get("/", (req, res) => {
-    res.send("Hi");
-})
-
-app.get("/items/:vegetable", (req, res) => {
-    let veg = req.params.vegetable;
-    res.send(`Request for ${veg}\n`);
-})
+app.set("view engine", "ejs");
 
 
 app.use(
@@ -23,38 +13,21 @@ app.use(
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hi<\h1>")
-});
 
-app.post("/", (req, res) => {
-    console.log(req.body);
-    console.log(req.query);
-    res.send("POST Successfull!\n");
-});
-
-app.get('/user/:id', (req, res, next) => {
-  console.log('ID:', req.params.id)
-  next()
-}, (req, res, next) => {
-  res.send('User Info')
-})
-// handler for the /user/:id path, which prints the user ID
-app.get('/user/:id', (req, res, next) => {
-    res.send(req.params.id)
-})
-*/
-
-function fob(req, res) {
-    console.log(req.originalUrl);
-    res.send('Mango');
-}
 
 
 
 app.get("/", homeController.sendHi);
 
+app.use("/items", homeController.checkItems);
+
 app.get("/items/:vegetable", homeController.sendReqParam);
+
+app.post("/sign_up", homeController.userSignUpProcessor);
+
+app.get("/name", homeController.respondWithName);
+
+
 
 
 app.listen(port, () => {
