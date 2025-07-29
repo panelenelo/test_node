fdb2790492ed2119f6c86e81d1baee5c6d9ba098
panelenelo <panelenelo@gmail.com>
2025-07-29 18:50:29 -0300

    study 2025/07/2

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const port = 3000,
     app = express(),
     homeController = require("./controllers/homeController"),
     errorController = require("./controllers/errorController"),
-    layouts = require("express-ejs-layouts"),
-    routesMap = new Map();
+    layouts = require("express-ejs-layouts");
+
 
 app.set("view engine", "ejs");
 app.use(layouts);
